8e5b6267a773b307daeda982362b30eeb24434a3
CeresBarros <cbarros@mail.ubc.ca>
2022-03-04 13:48:14 -0800

    updates to Exampl1 modules

--- modules/temperature/temperature.R
+++ modules/temperature/temperature.R
@@ -5,16 +5,16 @@ defineModule(sim, list(
   name = "temperature",
   description = "Temperature simulator",
   keywords = c("temperature", "gaussian", "spatial"),
-  authors = person("Mr.", "Me", email = "mr.me@example.com", role = c("aut", "cre")),
+  authors = person("Me", email = "me@example.com", role = c("aut", "cre")),
   childModules = character(0),
-  version = list(SpaDES.core = "0.1.1.9011", speciesAbundance = "0.0.1"),
-  # spatialExtent = raster::extent(rep(NA_real_, 4)),
+  version = list(speciesAbundanceData = "0.0.0.9000"),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "temperature.Rmd"),
-  reqdPkgs = list("raster"),
-  parameters = rbind(
+  reqdPkgs = list("PredictiveEcology/SpaDES.core@development (>=1.0.10.9000)",
+                  "raster"),
+   parameters = bindrows(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
     defineParameter("simulationTimeStep", "numeric", 1, NA, NA, 
                     "This describes the simulation time step interval"),
@@ -23,11 +23,11 @@ defineModule(sim, list(
     defineParameter(".plotInterval", "numeric", 1, NA, NA,
                     "This describes the simulation time interval between plot events")
   ),
-  inputObjects = bind_rows(
+  inputObjects = bindrows(
     #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
     expectsInput("r", "RasterLayer", "Template raster")
   ),
-  outputObjects = bind_rows(
+  outputObjects = bindrows(
     #createsOutput("objectName", "objectClass", "output object description", ...),
     createsOutput("tempRasters",  "list", "List of raster layers of temperature at any given year")
   )
@@ -46,14 +46,14 @@ doEvent.temperature = function(sim, eventTime, eventType, debug = FALSE) {
       ## schedule future event(s)
       sim <- scheduleEvent(sim, eventTime = start(sim), moduleName = "temperature", eventType = "SimulTemp")
       sim <- scheduleEvent(sim, eventTime = P(sim)$.plotInitialTime, moduleName = "temperature", 
-                           eventType = "tempPlot", eventPriority = .normal()+0.5)
+                           eventType = "tempPlot", eventPriority = .normal() + 0.5)
     },
     SimulTemp = {
       ## do stuff for this event
       sim <- temperatureSim(sim)
       
       ## schedule future event(s)
-      sim <- scheduleEvent(sim, eventTime = time(sim)+ P(sim)$simulationTimeStep, moduleName = "temperature", 
+      sim <- scheduleEvent(sim, eventTime = time(sim) + P(sim)$simulationTimeStep, moduleName = "temperature", 
                            eventType = "SimulTemp")
     },
     tempPlot = {
@@ -62,7 +62,7 @@ doEvent.temperature = function(sim, eventTime, eventType, debug = FALSE) {
 
       ## schedule future event(s)
       sim <- scheduleEvent(sim, eventTime = time(sim) + P(sim)$.plotInterval, moduleName = "temperature", 
-                           eventType = "tempPlot", eventPriority = .normal()+0.5)
+                           eventType = "tempPlot", eventPriority = .normal() + 0.5)
     },
     warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
                   "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
@@ -100,7 +100,7 @@ temperaturePlot <- function(sim) {
 }
 
 .inputObjects <- function(sim) {
-  if(!suppliedElsewhere(sim$r)) {
+  if (!suppliedElsewhere("r")) {
     ## make template raster if not supplied elsewhere.
     sim$r <- raster(nrows = 100, ncols = 100, xmn = -50, xmx = 50, ymn = -50, ymx = 50)
   }
