37afad8e28480374512ad1b67b0055790e38a4ab
CeresBarros <cbarros@mail.ubc.ca>
2022-03-25 20:00:22 -0700

    minor improvements

--- _common.R
+++ _common.R
@@ -1,8 +1,11 @@
 # example R options set globally
-options(width = 60)
-
+options("width" = 60,
+        "repos" = c(CRAN = "https://cran.rstudio.com"))
 # example chunk options set globally
 knitr::opts_chunk$set(
-  comment = "#>",
-  collapse = TRUE
+  collapse = TRUE,
+  cache = 2, 
+  cache.rebuild = FALSE, 
+  tidy = TRUE, 
+  tidy.opts = list(width.cutoff = 60)
   )
