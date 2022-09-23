310c76df9e9ac13f8a236b3678f07f1835b67ce0
CeresBarros <cbarros@mail.ubc.ca>
2022-09-22 20:49:18 -0700

    define .libPaths in common.R (affects ALL .Rdms)

--- _common.R
+++ _common.R
@@ -1,3 +1,7 @@
+pkgPath <- file.path("packages", version$platform,
+                     paste0(version$major, ".", strsplit(version$minor, "[.]")[[1]][1]))
+.libPaths(pkgPath)   ## need to include.side = TRUE to use bookdown and rmarkdown
+
 # example R options set globally
 options("width" = 60,
         "repos" = c(CRAN = "https://cran.rstudio.com"))
