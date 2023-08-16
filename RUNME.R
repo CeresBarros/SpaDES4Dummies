4409bfc1800f4c8ea03f1c46beb162f69cdddae6
CeresBarros <ceres.barros@gov.bc.ca>
2023-08-16 12:46:20 -0700

    RUNME: fix pkg install/loads

--- RUNME.R
+++ RUNME.R
@@ -3,11 +3,12 @@
 ## ---------------------------------------
 
 ## Sets up project library and renders book
-options(repos = c(CRAN = "https://cloud.r-project.org"))
+options(repos = c("https://predictiveecology.r-universe.dev/", 
+                  CRAN = "https://cloud.r-project.org"))
 
 ## note that "rmarkdown", "bookdown", "htmlwidgets" need to be installed in the default
 ## libraries, because each .Rmd starts from a clean R session
-needPkgs <- c("rmarkdown", "bookdown", "htmlwidgets", "tinytex")
+needPkgs <- c("rmarkdown", "bookdown", "htmlwidgets", "tinytex", "git2r")
 needPkgs <- needPkgs[!needPkgs %in% installed.packages()] 
 for (pkg in needPkgs) {
   install.packages(pkg, dependencies = TRUE)
@@ -25,24 +26,22 @@ dir.create(pkgPath, recursive = TRUE)
 if (!"remotes" %in% installed.packages(lib.loc = pkgPath))
   install.packages("remotes")
 
-if (!"Require" %in% installed.packages(lib.loc = pkgPath) || 
-    packageVersion("Require", lib.loc = pkgPath) < "0.1.2") {
-  remotes::install_github("PredictiveEcology/Require@86254b17ad2392de5c9e4dae6dd06a194b69a169",
-                          upgrade = FALSE, force = TRUE)
+if (!"Require" %in% installed.packages(lib.loc = pkgPath) ||
+    packageVersion("Require", lib.loc = pkgPath) < "0.3.1") {
+  remotes::install_github("PredictiveEcology/Require@55ec169e654214d86be62a0e13e9a2157f1aa966",
+                          upgrade = FALSE)
 }
 
 ## use binary linux packages if on Ubuntu
 Require::setLinuxBinaryRepo()
 
-Require::Require(c("PredictiveEcology/SpaDES.project@a9fe467953ee4c3ca704a9781ab22e105ce342ba", 
-                   "SpaDES"), 
+Require::Require(c("PredictiveEcology/SpaDES.project@a9fe467953ee4c3ca704a9781ab22e105ce342ba"), 
                  require = FALSE, upgrade = FALSE, standAlone = TRUE)
 
 outs <- SpaDES.project::packagesInModules(modulePath = file.path("modules"))  ## gets list of module dependencies
-Require::Require(c(unname(unlist(outs)),
+Require::Require(c(unname(unlist(outs)), "SpaDES",
                    "bookdown", "DiagrammeR", "htmlwidgets", "geodata", "ggplot2", 
-                   "ggpubr", "rmarkdown", "rsvg", "achubaty/NLMR@HEAD",
-                   "PredictiveEcology/SpaDES.experiment@development"), 
+                   "ggpubr", "rmarkdown", "rsvg"), 
                  require = FALSE,   ## don't load packages
                  upgrade = FALSE,   ## don't upgrade dependencies
                  standAlone = TRUE) 
