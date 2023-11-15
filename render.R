8f6ea7af6ff8738592c85da09f2e89af3f8f0da1
CeresBarros <ceres.barros@gov.bc.ca>
2023-11-15 15:26:33 -0800

    GHA: give up on PDF

--- render.R
+++ render.R
@@ -2,4 +2,4 @@
 ## GHA RENDER SCRIPT
 ## ---------------------------------------------------
 
-quarto::quarto_render(as_job = FALSE)
+quarto::quarto_render(output_format = "html", as_job = FALSE)   ## Nov 2023, can't load figures in GHA when knitting to PDF 
